7eb6f968ffc1901b59200923daf8ad84c068c899
Sebastian Gonzalez <jusegofo@gmail.com>
2024-09-16 22:46:11 -0500

    [add] basic page Form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
    //Asi se puede crear un formulario de manera manual     ---- a mi me gusta mas esta
   // public myForm: FormGroup = new FormGroup({
@@ -24,11 +24,39 @@ export class BasicPageComponent {
     })
   }
 
+  ngOnInit(): void {
+
+  }
+
+  isValidField(field:string): boolean | null{
+    return this.myForm.controls[field].errors &&
+    this.myForm.controls[field].touched
+  }
+
+  getFieldError(field:string):string | null{
+    if (!this.myForm.controls[field]) return null
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+    return null;
+  }
+
   onSave():void{
-    if(this.myForm.invalid) return;
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched;
+      return;
+    }
     console.log(this.myForm.value);
 
-    this.myForm.reset();
+    this.myForm.reset({ price:0, inStorage:0});
   }
 
 }
